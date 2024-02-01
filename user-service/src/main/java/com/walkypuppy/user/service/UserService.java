e435e82291eb6717b5722b1915751ab05a203392
페퍼푸딩 <106955460+pepper-pudding@users.noreply.github.com>
2024-02-01 20:29:42 +0900

    run

--- user-service/src/main/java/com/walkypuppy/user/service/UserService.java
+++ user-service/src/main/java/com/walkypuppy/user/service/UserService.java
@@ -1,9 +1,7 @@
 package com.walkypuppy.user.service;
 
-import com.walkypuppy.user.dto.UserDTO;
 import com.walkypuppy.user.model.entity.User;
 import com.walkypuppy.user.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
